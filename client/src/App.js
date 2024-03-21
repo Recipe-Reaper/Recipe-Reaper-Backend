621c9ab0bf858ed3f76b153048c5519c2234c823
Gabriel Uebele <geu1@students.uwf.edu>
2024-03-21 11:24:13 -0500

    Client for testing the backend for login and groceries.

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,32 @@
-import { useState } from 'react';
+import React from 'react';
+import {BrowserRouter as Router, Routes,Route, Navigate} from "react-router-dom";
 import './App.css';
-import axios from 'axios';
-
+import Home from './Home.js';
+import Login from './Login.js';
+import Groceries from './Groceries.js';
 
 
 function App() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    function sendLoginRequest(e) {
-        e.preventDefault();
-        axios.post("http://localhost:8080/login", {username, password})
-    }
-
     return (
-	<div id="wrapper">
-	    <h1>Image goes here.</h1>
-	    <form onSubmit={sendLoginRequest}>
-            <label for="username">Username:</label>
-            <input type="input" 
-                value={username} onChange = {(e) => setUsername(e.target.value)}
-                name="username" id="username" />
-            <br/>
-			<br/>
-            <label for="password">Password:</label>
-            <input type="input" 
-                value={password} onChange={(e) => setPassword(e.target.value)} 
-                name="password" id="password" />
-            <br/>
-			<br/>
-            <input type="submit" value="Log In" />
-        </form>
-	</div>
-  );
+        <div id="wrapper">
+            <Router>
+                <Routes>
+                    <Route 
+                        path="/login"
+                        element={<Login />}
+                    />
+                    <Route 
+                        path="/groceries"
+                        element={<Groceries />}
+                    />
+                    <Route 
+                        path="*"
+                        element={<Home />}
+                    />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
---
+++ client/src/Home.js
@@ -0,0 +1,13 @@
+const Home = function() {
+    return(
+        <div id="wrapper">
+            <h1>Welcome to Recipe Reaper!</h1>
+            <br/>
+            <a href="/login">Login</a> 
+            <br/>
+            <a href="/groceries">Groceries</a> 
+        </div>
+    )
+}
+
+export default Home;
